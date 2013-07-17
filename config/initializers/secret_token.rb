3cfa3fb6c1269b105f38b0267889997bd5e47f42
blibotics <blibotics@gmail.com>
2013-07-17 15:49:49 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crowdfunder::Application.config.secret_key_base = 'bca28b1832f4813c70bed3ba8250ffe78e8da0a87eb5f4525e7da808f030941ee6148088aa60ebed4c63c2c7ecea45268fa8faaa263f58213c7b0c9d427374fa'
