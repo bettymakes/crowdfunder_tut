71883a8652b9f8e46f6f94f5f625229340088f3b
blibotics <blibotics@gmail.com>
2013-07-17 16:32:21 -0400

    Adding FactoryGirl for fixtures and Capybara for integration tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,3 +13,8 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
 end
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  Capybara.app = Crowdfunder::Application
+end
