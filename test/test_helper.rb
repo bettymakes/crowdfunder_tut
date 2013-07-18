75d21dd6e889270a95c1e9687a84afb8f75768c2
blibotics <blibotics@gmail.com>
2013-07-18 17:39:11 -0400

    Writing tests for login and logout

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,12 +14,28 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
 
+DatabaseCleaner.strategy = :truncation
+
 class ActionDispatch::IntegrationTest
   include Capybara::DSL
   Capybara.app = Crowdfunder::Application
 
+  Capybara.javascript_driver = :webkit
+  self.use_transactional_fixtures = false
+
   teardown do
+  DatabaseCleaner.clean
   Capybara.reset_sessions!    # Forget the (simulated) browser state
   Capybara.use_default_driver # Revert Capybara.current_driver to Capybara.default_driver
+  end
+
+  def setup_signed_in_user
+    pass = "this-is-a-password"
+    user = FactoryGirl.create :user, password: pass
+    visit 'sessions/new'
+
+    fill_in "email", with: user.email
+    fill_in "password", with: pass
+    click_button "Login"
   end
 end
