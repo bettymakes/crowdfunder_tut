878e398914f2f09f7efcb281e8ab401cb4f19cac
blibotics <blibotics@gmail.com>
2013-07-18 15:06:13 -0400

    Writing tests for errors on user registration form

--- test/integration/user_authentication_flows_test.rb
+++ test/integration/user_authentication_flows_test.rb
@@ -38,6 +38,9 @@ test "successful registration" do
     assert_equal users_path, current_path
     # ... no message saying "Account created" should appear
     assert page.has_no_content?("Account created")
+
+    # Should see "Try again" message on failure to register 
+    assert find('.alert:first').has_content?("Try again")
   end
 
 end
