a947a25c1c1371399e317268cbe6f84bbc31238e
blibotics <blibotics@gmail.com>
2013-07-18 12:30:01 -0400

    Updating test

--- test/integration/user_authentication_flows_test.rb
+++ test/integration/user_authentication_flows_test.rb
@@ -2,42 +2,41 @@ require 'test_helper'
 
 class UserAuthenticationFlowsTest < ActionDispatch::IntegrationTest
 
-  test "successful registration" do
+  test "successful registration" do 
     visit "/users/new"
     assert_equal new_user_path, current_path
 
-    #assert there is a link in the nav to Sign Up
-    assert find(".navbar").has_link?("Sign Up")
+    # Assert there is a link in the nav to Sign Up
+    assert find('.navbar').has_link?('Sign Up')
 
-    user = FactoryGirl.build(:user)
+    user = FactoryGirl.build(:user) 
 
-    #fill in the form with the info from user created by FactoryGirl
+    # Fill in the from with the info from user created by FactoryGirl
     fill_in "user[email]", :with => user.email
     fill_in "user[first_name]", :with => user.first_name
     fill_in "user[last_name]", :with => user.last_name
     fill_in "user[password]", :with => user.password
     click_button "Create Account"
 
-    #after submitting the form, should be redirected to root
+    # After submitting the form, should be redirected to root
     assert_equal root_path, current_path
-    #while a message says "Account Created"
+    # While a message says "Account Created"
     assert page.has_content?("Account created")
-    #and the nav no longer has a link to sign up but logout
-    assert find(".navbar").has_no_link?("Sign Up")
-    assert find(".navbar").has_link("Logout")
+    # And the nav no longer has a link to Sign Up but Logout
+    assert find('.navbar').has_no_link?('Sign Up')
+    assert find('.navbar').has_link?('Logout')
   end
 
-  test "failed resgistration" do
-    visit "user/new"
-    user = FactoryGirl.build(:user)
+  test "failed registration" do 
+    visit "/users/new"
+    user = FactoryGirl.build(:user) 
 
-    #invalid form submission ...
+    # Invalid form submission ...
     fill_in "user[email]", :with => user.email
     click_button "Create Account"
-    #should redirect to users_path and ...
+    # Should redirect to users_path and ...
     assert_equal users_path, current_path
-    #... no message saying "Account created" should appear
+    # ... no message saying "Account created" should appear
     assert page.has_no_content?("Account created")
   end
-
 end
